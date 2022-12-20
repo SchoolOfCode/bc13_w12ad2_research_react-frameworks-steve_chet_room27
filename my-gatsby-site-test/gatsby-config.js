f24e0bc668ea7ffc9d5d270989749fb920a9fa75
Sunbearian <stigcollins@icloud.com>
2022-12-20 14:58:54 +0000

    Finish page 2, working on plugins

--- my-gatsby-site-test/gatsby-config.js
+++ my-gatsby-site-test/gatsby-config.js
@@ -2,9 +2,13 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
-  siteMetadata: {
-    title: `My Gatsby Site`,
-    siteUrl: `https://www.yourdomain.tld`,
-  },
-  plugins: [],
-}
+	siteMetadata: {
+		title: `My Gatsby Site`,
+		siteUrl: `https://www.yourdomain.tld`,
+	},
+	plugins: [
+		"gatsby-plugin-image",
+		"gatsby-plugin-sharp",
+		"gatsby-source-filesystem",
+	],
+};
